da0eb00d15dd92595a69b6d4fede834b075c0ad7
Liarum <lar404@naver.com>
2020-01-15 14:40:56 +0900

    create commento api service

---
+++ src/services/CommentoService.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const baseUrl = "https://1rcwozojf0.execute-api.ap-northeast-2.amazonaws.com/production"
+
+export default {
+    getPostList(params) {
+        const requestUrl = baseUrl + "/api/list"
+        axios.get(requestUrl, {
+            params : params
+        })
+            .then(res => {
+                console.log(res.data.list.data)
+            })
+            .catch(err => console.log(err))
+    },
+
+    getFilterCategory() {
+        axios.get(baseUrl + "/api/category")
+            .then(res => {
+                console.log(res.data.list)
+            })
+            .catch(err => console.log(err))
+    },
+
+    getAdList(params) {
+        const requestUrl = baseUrl + "/api/ads"
+        axios.get(requestUrl, {
+            params: params
+        })
+            .then(res => {
+                console.log(res.data.list.data)
+            })
+            .catch(err => console.log(err))
+
+    },
+
+    getPostDetail(params) {
+        const requestUrl = baseUrl + "/api/view"
+        axios.get(requestUrl, {
+            params: params
+        })
+            .then(res => {
+                console.log(res.data.info)
+            })
+            .catch(err => console.log(err))
+
+    },
+}
