9de80cac676634566f5db05282dccff31152967a
Liarum <lar404@naver.com>
2020-01-24 21:34:38 +0900

    implement axios code of commento api service

--- src/services/CommentoService.js
+++ src/services/CommentoService.js
@@ -2,46 +2,73 @@ import axios from 'axios'
 
 const baseUrl = "https://1rcwozojf0.execute-api.ap-northeast-2.amazonaws.com/production"
 
+const qs = require('qs');
+
 export default {
-    getPostList(params) {
+    getPosts(params) {
+        
+        // console.log("getPosts", params)
+
+        /*
+           params:
+           - page: int
+           - ord: string ("asc" or "desc")
+           - category: array
+           - limit: int (perPage)
+        */
         const requestUrl = baseUrl + "/api/list"
-        axios.get(requestUrl, {
-            params : params
+        return axios.get(requestUrl, {
+            'params' : params,
+            'paramsSerializer': (params) => {return qs.stringify(params) }
         })
             .then(res => {
-                console.log(res.data.list.data)
+                // console.log(res.data.list.data)
+                return res.data.list.data
             })
             .catch(err => console.log(err))
     },
 
-    getFilterCategory() {
-        axios.get(baseUrl + "/api/category")
+    getAllCategory() {
+        return axios.get(baseUrl + "/api/category")
             .then(res => {
-                console.log("service", res.data.list)
                 return res.data.list
             })
             .catch(err => console.log(err))
     },
 
-    getAdList(params) {
+    getAds(params) {
+        // console.log("getAds", params)
+        /*
+           params:
+           - page: int
+           - limit: int
+        */
         const requestUrl = baseUrl + "/api/ads"
-        axios.get(requestUrl, {
+        return axios.get(requestUrl, {
             params: params
         })
             .then(res => {
-                console.log(res.data.list.data)
+                // console.log(res.data.list.data)
+                return res.data.list.data
             })
             .catch(err => console.log(err))
 
     },
 
-    getPostDetail(params) {
+    getPostDetail(postId) {
+        /*
+           params:
+           - id: int
+        */
         const requestUrl = baseUrl + "/api/view"
-        axios.get(requestUrl, {
-            params: params
+        return axios.get(requestUrl, {
+            params: {
+                "id" : postId
+            }
         })
             .then(res => {
-                console.log(res.data.info)
+                // console.log(res.data.info)
+                return res.data.info
             })
             .catch(err => console.log(err))
 
